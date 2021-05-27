63b928d9b367b02941dff7aa09a0216e61e11898
Dylan Baird <Baird.Dylan@gmail.com>
2021-05-26 21:23:34 -0400

    final formatting based on user inputs stored in array works.

--- assignment-2/VaccineTest.java
+++ assignment-2/VaccineTest.java
@@ -6,17 +6,25 @@ public class VaccineTest {
     public static void main(String[] args) {
         int userInput;
         Scanner reader = new Scanner(System.in);
-
         boolean valid = false;
+
+        int[] userInputs = new int[3];
+        String[][] vacTypeDose = {
+            { "Pfizer", "m-RNA", "2" },
+            { "Moderna", "m-RNA", "2" }
+        };
+
+
 
         
         do {      
             try {
+                System.out.println("Please choose from the following vaccines: ");
+                for (int i = 0; i < vacTypeDose[0].length - 1; i++) {
+                    System.out.println(vacTypeDose[0][i]);
+                }
                 System.out.println("Enter a number:");
-                userInput = reader.nextInt();
-                
-        
-
+                userInputs[0] = reader.nextInt();
 
                 valid = true;
             }
@@ -26,6 +34,13 @@ public class VaccineTest {
             }
         }
         while (!valid);
+
+        /*
+        Handle final formatting.
+        */
+        // if (userInputs[0] == 1) { 
+        System.out.println(vacTypeDose[userInputs[0]][0] + vacTypeDose[userInputs[0]][1] + vacTypeDose[userInputs[0]][2]);
+        
         System.out.println("Thank you for using this program.");        
     }
 }
